17dfa4e449f79dcc8b000022fce5f2cc31e35b4b
Nechar <joshinechar@gmail.com>
2020-01-18 13:34:50 +1100

    feature: buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import './App.css';
 
+import Button from 'react-bootstrap/Button';
+
 function App() {
-  return <div>app works!</div>;
+  return (
+    <div>
+      <Button>Feed me</Button>
+      <Button>Put me to bed</Button>
+      <Button>Pet me</Button>
+      <Button>Take me to the toilet</Button>
+      <Button>Clean it up</Button>
+      <Button>Take me to shower</Button>
+    </div>
+  );
 }
 
 export default App;
