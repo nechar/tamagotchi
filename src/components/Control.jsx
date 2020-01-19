7c4232788a5faeb6945e0d2a551ea633c73a5e86
Nechar <joshinechar@gmail.com>
2020-01-19 12:20:59 +1100

    feature: Control setup

--- src/components/Control.jsx
+++ src/components/Control.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import { ONE_TOMAGOTCHI_HOUR } from '../config';
+
+/* Global State Management */
+import { useGlobalState } from '../store';
 
 const Control = () => {
+  const [, setSleepy] = useGlobalState('sleepy');
+  const [, setHunger] = useGlobalState('hunger');
+
+  function feedMe() {
+    setHunger(0);
+  }
+
+  function putMeToBed() {
+    setSleepy(100);
+    setTimeout(() => {
+      setSleepy(0);
+    }, ONE_TOMAGOTCHI_HOUR * 8);
+  }
+
   return (
     <div>
       <div className="center-text-align">
-        <Button className="m-1">Feed me</Button>
+        <Button className="m-1" onClick={feedMe}>
+          Feed me
+        </Button>
         <br />
-        <Button className="m-1">Feed me</Button>
-        <br />
-        <Button className="m-1">Put me to bed</Button>
-        <br />
-        <Button className="m-1">Take me to the toilet</Button>
-        <br />
-        <Button className="m-1">Take me to shower</Button>
-        <br />
-        <Button className="m-1">Clean it up</Button>
+        <Button className="m-1" onClick={putMeToBed}>
+          Put me to bed
+        </Button>
       </div>
     </div>
   );
